8b45d813e1a2260029ef5986ed36a5c2010acee1
TrudovMykhailo <156135082+SadAndAlone@users.noreply.github.com>
2025-01-14 19:09:42 +0100

    Add files via upload

---
+++ docs/html/search/files_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['integral_5fcalculator_2ecpp_0',['integral_calculator.cpp',['../integral__calculator_8cpp.html',1,'']]],
+  ['integral_5fcalculator_2eh_1',['integral_calculator.h',['../integral__calculator_8h.html',1,'']]]
+];
---
+++ docs/html/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['calculate_5fpartial_5fintegral_0',['calculate_partial_integral',['../integral__calculator_8cpp.html#a12747e5c4f8425e67837dd0b239d4c47',1,'calculate_partial_integral(int start, int end, int total_intervals):&#160;integral_calculator.cpp'],['../integral__calculator_8h.html#a12747e5c4f8425e67837dd0b239d4c47',1,'calculate_partial_integral(int start, int end, int total_intervals):&#160;integral_calculator.cpp'],['../main_8cpp.html#a12747e5c4f8425e67837dd0b239d4c47',1,'calculate_partial_integral(int start, int end, int total_intervals):&#160;main.cpp']]],
+  ['calculate_5fpi_1',['calculate_pi',['../integral__calculator_8cpp.html#a6513ad64ffb15f438ae32719026f9b43',1,'calculate_pi(int total_intervals, int num_threads):&#160;integral_calculator.cpp'],['../integral__calculator_8h.html#a6513ad64ffb15f438ae32719026f9b43',1,'calculate_pi(int total_intervals, int num_threads):&#160;integral_calculator.cpp']]]
+];
